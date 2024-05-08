b43a3d86e9b00eae31b5c5023c884337dec80a3f
marmarsol4 <marmarsol4@alum.us.es>
2024-05-08 12:18:02 +0200

    Ficheros del PAI 3

---
+++ runServer.bat
@@ -0,0 +1,5 @@
+@echo off
+
+javac MsgSSLServerSocket.java
+
+java "-Djavax.net.ssl.keyStore=C:\SSLStore\keystore.jks" "-Djavax.net.ssl.keyStorePassword=pai3st1" -classpath ".;sqlite-jdbc-3.45.2.0.jar;slf4j-api-1.7.36.jar" MsgSSLServerSocket
