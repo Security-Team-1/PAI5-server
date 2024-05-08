2fd3a238db954ac3b75e0c3cb1fbc8916b45c286
marmarsol4 <marmarsol4@alum.us.es>
2024-05-08 13:29:13 +0200

    Intento de servidor

--- MsgSSLServerSocket.java
+++ MsgSSLServerSocket.java
@@ -3,31 +3,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+
 import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
 import java.util.Base64;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
 
 public class MsgSSLServerSocket {
-	
-	private static final String secretKey = "ST1PAI3-secretKey";
-	private static final int THREAD_POOL_SIZE = 300;
 
-	// DB operations
-	public static Connection createConnection() {
+	private static PrivateKey serverPrivateKey;
+	//private static PublicKey serverPublicKey;
+
+	private static Connection createConnection() {
 		
 		Connection conn = null;
 		Statement statement = null;
@@ -38,12 +50,14 @@ public class MsgSSLServerSocket {
 			statement.setQueryTimeout(30);  
 
 			statement.executeUpdate("drop table if exists users");
-			statement.executeUpdate("create table users (username string unique, password string)");
+			statement.executeUpdate("create table users (numCliente string unique, clavePublica string unique)");
 			System.out.println("Tabla users creada exitosamente.");
 
-			statement.executeUpdate("drop table if exists transactions");
-			statement.executeUpdate("create table transactions (username string, transaction_value string)");
-			System.out.println("Tabla transactions creada exitosamente.");
+			statement.executeUpdate("drop table if exists orders");
+			statement.executeUpdate("create table orders (numCliente number, numCamas number, numMesas number, numSillas number, numSillones number)");
+			System.out.println("Tabla orders creada exitosamente.");
+
+			generateKeysForServer();
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -51,15 +65,15 @@ public class MsgSSLServerSocket {
 		return conn;
 	}
 
-	public static void insertUser(Connection conn, String username, String password) {
+	private static void insertUser(Connection conn, String numCliente, String clavePublica) {
 
 		PreparedStatement preparedStatement = null;
 
 		try {		
-			String insertSQL = "INSERT INTO users (username, password) VALUES (?, ?)";
+			String insertSQL = "INSERT INTO users (numCliente, clavePublica) VALUES (?, ?)";
 			preparedStatement = conn.prepareStatement(insertSQL);
-			preparedStatement.setString(1, username);
-			preparedStatement.setString(2, password);
+			preparedStatement.setString(1, numCliente);
+			preparedStatement.setString(2, clavePublica);
 			preparedStatement.executeUpdate();
 		
 		} catch (SQLException e) {
@@ -67,15 +81,18 @@ public class MsgSSLServerSocket {
 		}
 	}
 
-	public static void insertTransaction(Connection conn, String username, String transaction) {
+	private static void insertTransaction(Connection conn, String numCliente, Integer numCamas, Integer numMesas, Integer numSillas, Integer numSillones) {
 
 		PreparedStatement preparedStatement = null;
 
 		try {		
-			String insertSQL = "INSERT INTO transactions (username, transaction_value) VALUES (?, ?)";
+			String insertSQL = "INSERT INTO transactions (numCliente, numCamas, numMesas, numSillas, numSillones) VALUES (?, ?, ?, ?, ?)";
 			preparedStatement = conn.prepareStatement(insertSQL);
-			preparedStatement.setString(1, username);
-			preparedStatement.setString(2, transaction);
+			preparedStatement.setString(1, numCliente);
+			preparedStatement.setInt(2, numCamas);
+			preparedStatement.setInt(3, numMesas);
+			preparedStatement.setInt(4, numSillas);
+			preparedStatement.setInt(5, numSillones);
 			preparedStatement.executeUpdate();
 		
 		} catch (SQLException e) {
@@ -83,10 +100,26 @@ public class MsgSSLServerSocket {
 		}
 	}
 
-	public static void populateDatabase(Connection conn) {
+	private static void populateDatabase(Connection conn) {
+
 		try {
-			for (int i = 1; i <= 300; i++) {
-				insertUser(conn, "user" + i, "password" + i);
+			for (int i = 1; i <= 10; i++) {
+
+				String publicKeyStr = null;
+				KeyPairGenerator keyPairGenerator;
+
+				try {
+					keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+					keyPairGenerator.initialize(2048);
+					KeyPair keyPair = keyPairGenerator.generateKeyPair();
+					PublicKey publicKey = keyPair.getPublic();
+					publicKeyStr = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+
+				} catch (NoSuchAlgorithmException e) {
+					e.printStackTrace();
+				}
+
+				insertUser(conn, "user" + i, publicKeyStr);
 			}
 			Statement statement = conn.createStatement();
 			ResultSet rs = statement.executeQuery("SELECT COUNT(*) FROM users");
@@ -96,10 +129,29 @@ public class MsgSSLServerSocket {
 		}
 	}
 
-	public static boolean verifyUserExists(Connection conn, String username, String password) {
+	private static void generateKeysForServer() {
+		
+		KeyPairGenerator keyPairGenerator;
+		try {
+			keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			KeyPair keyPair = keyPairGenerator.generateKeyPair();
+	
+			//PublicKey publicKey = keyPair.getPublic();
+			PrivateKey privateKey = keyPair.getPrivate();
+	
+			MsgSSLServerSocket.serverPrivateKey = privateKey;
+			//MsgSSLServerSocket.serverPublicKey = publicKey;
+
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static boolean verifyUserExists(Connection conn, String numCliente, String clavePublica) {
 		try {
 			Statement statement = conn.createStatement();
-			ResultSet rs = statement.executeQuery("SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'");
+			ResultSet rs = statement.executeQuery("SELECT * FROM users WHERE numCliente = '" + numCliente + "' AND clavePublica = '" + clavePublica + "'");
 			return rs.next(); // true if there is a row, false if not
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -111,7 +163,7 @@ public class MsgSSLServerSocket {
 	private static boolean verifyHMAC(String message, String receivedHMAC) {
 		try {
 			Mac mac = Mac.getInstance("HmacSHA256");
-			byte[] secretKeyBytes = secretKey.getBytes();
+			byte[] secretKeyBytes = serverPrivateKey.getEncoded();
 			SecretKeySpec secretKey = new SecretKeySpec(secretKeyBytes, "HmacSHA256");
 			mac.init(secretKey);
 
@@ -125,17 +177,35 @@ public class MsgSSLServerSocket {
 		}
 	}
 
-	private static boolean verifyRepeatedTransaction(Connection conn, String username, String transaction) {
+	private static Boolean verifySignature(Connection conn, String msg, String numCliente, String firma) {
+
 		try {
 			Statement statement = conn.createStatement();
-			ResultSet rs = statement.executeQuery("SELECT * FROM transactions WHERE username = '" + username + "' AND transaction_value = '" + transaction + "'");
-			return rs.next();
-		} catch (SQLException e) {
+			String publicKeyStr = statement.executeQuery("SELECT clavePublica FROM users WHERE numCliente = '" + numCliente.trim()).getString(0);
+			byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyStr);
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			PublicKey publicKey;
+			
+			try {
+				publicKey = keyFactory.generatePublic(new X509EncodedKeySpec(publicKeyBytes));
+				Signature sg = Signature.getInstance("SHA256withRSA");
+				sg.initVerify(publicKey);
+				sg.update(msg.getBytes());
+				return sg.verify(firma.getBytes());
+			} catch (InvalidKeySpecException e) {
+				e.printStackTrace();
+				return false;
+			} catch (SignatureException e) {
+				e.printStackTrace();
+				return false;
+			}
+	
+		} catch (SQLException | NoSuchAlgorithmException | InvalidKeyException e) {
 			e.printStackTrace();
 			return false;
 		}
 	}
-	
+
 	public static void main(String[] args) {
         
 		try {
@@ -144,8 +214,7 @@ public class MsgSSLServerSocket {
 
             SSLServerSocketFactory factory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
             SSLServerSocket serverSocket = (SSLServerSocket) factory.createServerSocket(3343);
-
-            ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+			ExecutorService executorService = Executors.newCachedThreadPool();
 
             while (true) {
                 System.err.println("Waiting for connection...");
@@ -175,21 +244,27 @@ public class MsgSSLServerSocket {
                 String msg = input.readLine();
                 String[] parts = msg.split(",");
 
-                String username = parts[0].trim();
-                String password = parts[1].trim();
-                String message = parts[2].trim();
-                String receivedHMAC = parts[3].trim();
+                String numCliente = parts[0].trim();
+				String clavePublica = parts[1].trim();
 
-                if (verifyUserExists(conn, username, password) && verifyHMAC(message, receivedHMAC)) {
-                    if (!verifyRepeatedTransaction(conn, username, message)) {
-                        insertTransaction(conn, username, message);
+				Integer numCamas = Integer.parseInt(parts[2].trim());
+				Integer numMesas = Integer.parseInt(parts[3].trim());
+				Integer numSillas = Integer.parseInt(parts[4].trim());
+				Integer numSillones = Integer.parseInt(parts[5].trim());
+
+				String macRecibida = parts[6].trim();
+				String firma = parts[7].trim();
+
+                if (verifyUserExists(conn, numCliente, clavePublica) && verifyHMAC(msg, macRecibida)) {
+                    if (!verifySignature(conn, msg, numCliente, firma)) {
+                        insertTransaction(conn, numCliente, numCamas, numMesas, numSillas, numSillones);
                         output.println("Transaccion exitosa. El mensaje ha sido almacenado en el servidor");
                     } else {
                         output.println("Transaccion repetida. El mensaje NO ha sido almacenado en el servidor");
                     }
-                } else if (!verifyUserExists(conn, username, password)) {
+                } else if (!verifyUserExists(conn, numCliente, clavePublica)) {
                     output.println("Usuario no reconocido/autenticado. El mensaje NO ha sido almacenado en el servidor");
-                } else if (!verifyHMAC(message, receivedHMAC)) {
+                } else if (!verifyHMAC(msg, macRecibida)) {
                     output.println("Mensaje no integro. El mensaje NO ha sido almacenado en el servidor");
                 }
 
--- runServer.bat
+++ runServer.bat
@@ -2,4 +2,4 @@
 
 javac MsgSSLServerSocket.java
 
-java "-Djavax.net.ssl.keyStore=C:\SSLStore\keystore.jks" "-Djavax.net.ssl.keyStorePassword=pai3st1" -classpath ".;sqlite-jdbc-3.45.2.0.jar;slf4j-api-1.7.36.jar" MsgSSLServerSocket
+java "-Djavax.net.ssl.keyStore=C:\SSLStore\keystore.jks" "-Djavax.net.ssl.keyStorePassword=pai5st1" -classpath ".;sqlite-jdbc-3.45.2.0.jar;slf4j-api-1.7.36.jar" MsgSSLServerSocket
